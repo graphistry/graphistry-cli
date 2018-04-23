75e769aae091d88710a4d513d3e12d334e7973db
Miller Hooks <miller@graphistry.com>
2018-04-23 13:51:27 -0500

    dep management

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     install_requires=install_requirements,
     entry_points='''
         [console_scripts]
-        graphistry=graphistry.main
+        graphistry=graphistry.main:main
     ''',
     classifiers=[
         'Intended Audience :: Developers',
