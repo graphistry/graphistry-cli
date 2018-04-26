d1d41949b104dddd8fbd84b0dfb6c17b24fd6d56
Miller Hooks <miller@graphistry.com>
2018-04-26 00:35:56 -0500

    setting up cli arguments

--- graphistry/main.py
+++ graphistry/main.py
@@ -4,35 +4,37 @@ from prompt_toolkit import prompt
 
 from graphistry.widgets import revisionist_commit_history_html
 from graphistry.menus import MainNav
-from graphistry.commands import __Commands__
+from graphistry.commands import __Commands__, __CommandsMeta__
 
 import click
 
 
 @click.command()
 @click.option('--headless', '-h', multiple=True)
-def main(headless=None):
-    while True:
-        toolbar_message = revisionist_commit_history_html()
+def main(headless=''):
+    if headless:
+        click.secho(__CommandsMeta__[headless], fg='yellow')
+        if headless in __Commands__:
+            cmd = MainNav.get_command(headless)
+            cmd()
+    else:
+        while True:
+            toolbar_message = revisionist_commit_history_html()
 
-        try:
-            if not headless:
+            try:
                 select = prompt('graphistry>> ',
                                 completer=MainNav().get_completer(),
                                 bottom_toolbar=toolbar_message,
                                 complete_while_typing=True,
                                 history=None)
-            else:
-                select = headless
 
-            if select in __Commands__:
-                cmd = MainNav.get_command(select)
-                cmd()
-
-        except EOFError:
-            break  # Control-D pressed.
-        except KeyboardInterrupt:
-            break
+                if select in __Commands__:
+                    cmd = MainNav.get_command(select)
+                    cmd()
+            except EOFError:
+                break  # Control-D pressed.
+            except KeyboardInterrupt:
+                break
 
 
 if __name__ == '__main__':
