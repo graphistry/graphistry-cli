3f2902597276ffc294be31818b499656b6382136
Leo Meyerovich <leo@graphistry.com>
2018-05-01 22:43:35 -0700

    feature(cli config clearer):   — redo order and headings and text   — skip connector settings for skipped connectors   — report written file location

--- graphistry/config.py
+++ graphistry/config.py
@@ -168,11 +168,10 @@ class Graphistry(object):
 
         
         # Graphistry API Key Generation
-        click.secho("[graphitry] API Key Settings. [Hash algorithm is 'aes-256-cbc'.]", fg="yellow")
-        click.secho("[graphitry] [If you choose nothing for your salt or canary they will be generated for you.", fg="yellow")
-
-        api_canary = prompt('Hash Canary string: ', bottom_toolbar=toolbar_quip, history=None)
-        api_secret = prompt('Your Secret string: ', bottom_toolbar=toolbar_quip, history=None)
+        click.secho("[graphistry] Configure API key generation. [Hash algorithm is 'aes-256-cbc'.]", fg="yellow")
+        
+        api_canary = prompt('Hash Canary string (enter to autogenerate): ', bottom_toolbar=toolbar_quip, history=None)
+        api_secret = prompt('Your Secret string (enter to autogenerate): ', bottom_toolbar=toolbar_quip, history=None)
 
         if api_canary == '':
             self.config.api_canary.value = id_generator(10)
@@ -183,36 +182,42 @@ class Graphistry(object):
         #self.config.graphistry_key.value = prompt('You supplied Graphisty key: ',
         #                                           bottom_toolbar=toolbar_quip, history=None)
 
+
+        # Elasticsearch
+        click.secho("[graphistry] Configure connectors", fg="yellow")
+        self.config.es_host.value = prompt('Your Elasticsearch Host (enter to skip): ',
+                                           bottom_toolbar=toolbar_quip, history=None)
+        if self.config.es_host.value != '':
+            self.config.es_port.value = prompt('Your Elasticsearch Port [default: 9200]: ',
+                                               bottom_toolbar=toolbar_quip, history=None)        
+
+
+        # Splunk
+        self.config.splunk_host.value = prompt('Your Splunk Host (enter to skip): ',
+                                               bottom_toolbar=toolbar_quip, history=None)
+        if self.config.splunk_host.value != '':
+            self.config.splunk_port.value = prompt('Your Splunk Port [default: 3000]: ',
+                                                   bottom_toolbar=toolbar_quip, history=None)
+            self.config.splunk_user.value = prompt('Your Splunk Username: ',
+                                                   bottom_toolbar=toolbar_quip, history=None)
+            self.config.splunk_password.value = prompt('Your Splunk Password: ',
+                                                       bottom_toolbar=toolbar_quip, history=None, is_password=True)
+
+        click.secho("[graphistry] Configure networking", fg="yellow")
+
+        # Pivotapp->ETL/Vizapp
         self.config.graphistry_host.value = prompt('Your FQDN for this deployment [e.g., graphistry.yourcompany.com]: ',
                                                    bottom_toolbar=toolbar_quip, history=None)
 
-        # Elasticsearch
-        click.secho("[graphistry ] Elasticsearch and Splunk Config", fg="yellow")
-        self.config.es_host.value = prompt('Your Elasticsearch Host: ',
-                                           bottom_toolbar=toolbar_quip, history=None)
-        self.config.es_port.value = prompt('Your Elasticsearch Port [default: 9200]: ',
-                                           bottom_toolbar=toolbar_quip, history=None)
-
-        # Splunk
-        self.config.splunk_host.value = prompt('Your Splunk Host: ',
-                                               bottom_toolbar=toolbar_quip, history=None)
-        self.config.splunk_port.value = prompt('Your Splunk Port [default: 3000]: ',
-                                               bottom_toolbar=toolbar_quip, history=None)
-        self.config.splunk_user.value = prompt('Your Splunk Username: ',
-                                               bottom_toolbar=toolbar_quip, history=None)
-        self.config.splunk_password.value = prompt('Your Splunk Password: ',
-                                                   bottom_toolbar=toolbar_quip, history=None, is_password=True)
-
-        click.secho("[graphistry ] Network Config", fg="yellow")
         # Ip Whitelist
         self.config.ip_internal_accept_list.value = prompt('Your Internal IP Accept Whitelist (beyond typical RFC 1918)'
                                                            ', ex:["127.0.0.1", "10.*"]',
                                                            bottom_toolbar=toolbar_quip, history=None)
 
         # Http Ingress
-        self.config.http_user.value = prompt('Pivotapp HTTP Ingress Username: ',
+        self.config.http_user.value = prompt('HTTP Ingress Username: ',
                                              bottom_toolbar=toolbar_quip, history=None)
-        password = prompt('Pivotapp HTTP Ingress Password: ',
+        password = prompt('HTTP Ingress Password: ',
                           bottom_toolbar=toolbar_quip,
                           history=None,
                           is_password=True)
@@ -221,8 +226,9 @@ class Graphistry(object):
                                                          capture=True)
 
         # AWS
-        self.config.s3_access.value = prompt('AWS Access Key ID: ', bottom_toolbar=toolbar_quip, history=None)
-        self.config.s3_secret.value = prompt('AWS Access Key Secret: ', bottom_toolbar=toolbar_quip, history=None)
+        self.config.s3_access.value = prompt('AWS Access Key ID (enter to skip): ', bottom_toolbar=toolbar_quip, history=None)
+        if self.config.s3_access.value != '':
+            self.config.s3_secret.value = prompt('AWS Access Key Secret: ', bottom_toolbar=toolbar_quip, history=None)
 
         self.save_config()
         self.write_configs()
@@ -255,5 +261,7 @@ class Graphistry(object):
 
         if self.config:
             self.config.json.dump(self.config_file, with_defaults=True)
+            print("Wrote config:", self.config_file)
+
 
 
