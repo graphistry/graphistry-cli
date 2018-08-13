d8662049c5b550ec112c985ce2c007f9216f9a8b
lmeyerov <lmeyerov@gmail.com>
2018-08-13 00:22:43 -0700

    revert(inline docker test needs sudo)

--- graphistry/bootstrap/ubuntu-cuda9.2/40-nvidia-docker.sh
+++ graphistry/bootstrap/ubuntu-cuda9.2/40-nvidia-docker.sh
@@ -16,7 +16,7 @@ sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
 
-sudo docker run --runtime=nvidia --rm nvidia/cuda nvidia-smi
+docker run --runtime=nvidia --rm nvidia/cuda nvidia-smi
 
 sudo echo -e "{\n    \"default-runtime\": \"nvidia\",\n    \"runtimes\": {\n        \"nvidia\": {\n            \"path\": \"/usr/bin/nvidia-container-runtime\",\n            \"runtimeArgs\": []\n        }\n    }\n}" > /etc/docker/daemon.json
 
