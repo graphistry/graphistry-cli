f48d82def1578d63afc058fcec0bad8b3b635490
Leo Meyerovich <leo@graphistry.com>
2018-09-27 20:03:20 -0700

    docs(add pdf gen)

---
+++ docs/build.sh
@@ -0,0 +1,17 @@
+### Generates docs/readme.pdf and docs/hardware-software.pdf
+
+docker run --rm -it -v $PWD/..:/source jagregory/pandoc --toc -V documentclass=report -s \
+	README.md \
+	docs/configure.md \
+	docs/hardware-software.md \
+	docs/aws.md \
+	docs/azure.md \
+	docs/debug-faq.md \
+	docs/debug-logs.md \
+	docs/debug-container-networking.md \
+	-o docs/readme.pdf
+
+
+docker run --rm -it -v $PWD/..:/source jagregory/pandoc --toc -V documentclass=report -s docs/hardware-software.md -o docs/hardware-software.pdf
+
+ls -al readme.pdf hardware-software.pdf
